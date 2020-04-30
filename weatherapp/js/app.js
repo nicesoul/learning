ddb104fe286a06433c0664d058d35366fd3a5097
nicesoul <vadimka.wonderland@gmail.com>
2020-04-30 08:39:20 +0200

    Finished basic site (weather API)
    Working but need refactoring. Also should change alerts() for modals of kind not to be so annoying ))

--- weatherapp/js/app.js
+++ weatherapp/js/app.js
@@ -1,33 +1,12 @@
-//Pseudocode goes like this:
-
-
-// City name: jsonResponse.name
-// Describe of weather: jsonResponse.weather[0].main + description
-// Weather Icon = `http://openweathermap.org/img/wn/${jsonResponse.weather[0].icon}@2x.png`
-// Pressure = jsonResponse.main.pressure
-// Humidity= jsonResponse.main.humidity
-// Wind Speed = jsonResponse.wind.speed
-
-// WeatherByHours: https://api.openweathermap.org/data/2.5/forecast?q=Brussels&appid=f6211bc24c258c57f7a7fba887afdd17
-
-// let responseofWeatherHours =  data.list
-
-// 9clock =  data.list[0]
-// 12clcok = data.list[1]
-// 15 = data.list[2]
-
-
 const result = document.querySelector('.result');
-const API_key = 'appid=f6211bc24c258c57f7a7fba887afdd17';
+const API_key = 'appid=f6211bc24c258c57f7a7fba887afdd17'; // Atilla's key
 const API_currentWeather = 'http://api.openweathermap.org/data/2.5/weather';
 const API_forecast = 'https://api.openweathermap.org/data/2.5/forecast';
 const searchinput = document.querySelector('#input_search');
-// const editButton = document.querySelector('._edit');
 const searchButton = document.querySelector('._send');
-searchButton.addEventListener('click', sendSearch);
-// editButton.addEventListener('click', startSearch);
+searchButton.addEventListener('click', startSearch);
 
-//constants for overrideng initial HTML with real data? or object destructuring?
+//constants for overrideng initial HTML with real data
 var divCity = document.querySelector('.city');
 var divDescription = document.querySelector('.description');
 var divDate = document.querySelector('.date');
@@ -39,60 +18,85 @@ var divWindSpeed = document.querySelector('.windspeed');
 var divHourlyIcon = document.querySelectorAll('.hourly img');
 var divHourlyTemp = document.querySelectorAll('.hourly span');
 divDate.innerText = new Date().toLocaleDateString();
-console.log(divIcon);
 
+initialSearch();
+searchinput.focus();
+// activate search by hitting Enter
+searchinput.addEventListener("keyup", event => {
+    if (event.keyCode === 13) {
+        searchButton.click();}
+});
+
+
+function startSearch() {
+    let city = searchinput.value;
+    if (city.trim() === '') {
+        return alert('provide a city');
+    } else {
+        sendSearch()
+    }
+}
 
 function sendSearch() {
     let city = searchinput.value;
-    if (city.trim() === '') {
-        return alert('provide a city')
-    } else {
-        let API_URL1 = `${API_currentWeather}?q=${city}&${API_key}`;
-        // let API_URL2 = `${API_forecast}?q=${city}&${API_key}&cnt=6`;
-        // let KtC = `273.15).toFixed(0)` // KelvinToCelcium
-        fetch(API_URL1)
-            .then(response => response.json())
-            .then(console.log(response));
-            
-        
-        return 1;
-    }
-}
-/*
-.then(data => {
-    result.innerHTML = JSON.stringify(data);
-    divCity.innerText = data.name;
-    divDescription.innerText = data.weather[0].main;
-    divDate.innerText = new Date().toLocaleString();
-    divTemp.innerText = Number(data.main.temp - 273.15).toFixed(0);
-    let icon = data.weather[0].icon;
-    divIcon.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    divPressure.innerText = data.main.pressure + ' hPa';
-    divHumidity.innerText = data.main.humidity + ' %';
-    divWindSpeed.innerText = data.wind.speed + ' m/s';
-})
-fetch(API_URL2)
-    .then(response => response.json())
-    .then(data => {
-        result.innerHTML = JSON.stringify(data);
-        divHourlyTemp[0].innerText = data.list[0].dt_txt.slice(12, 16) + ' - ' + Number(data.list[0].main.temp - 273.15).toFixed(0) + '°C';
-        divHourlyTemp[1].innerText = data.list[1].dt_txt.slice(12, 16) + ' - ' + Number(data.list[1].main.temp - 273.15).toFixed(0) + '°C';
-        divHourlyTemp[2].innerText = data.list[2].dt_txt.slice(12, 16) + ' - ' + Number(data.list[2].main.temp - 273.15).toFixed(0) + '°C';
-        divHourlyTemp[3].innerText = data.list[3].dt_txt.slice(12, 16) + ' - ' + Number(data.list[3].main.temp - 273.15).toFixed(0) + '°C';
-        divHourlyTemp[4].innerText = data.list[4].dt_txt.slice(12, 16) + ' - ' + Number(data.list[4].main.temp - 273.15).toFixed(0) + '°C';
-    })
-}
-}
+    let API_URL1 = `${API_currentWeather}?q=${city}&${API_key}`;
+    let API_URL2 = `${API_forecast}?q=${city}&${API_key}&cnt=6`;
 
-function startSearch() {
-    searchinput.style.display = 'inline';
+    fetch(API_URL1)
+        .then(response => {
+            if (!response.ok) {
+                alert('city not found, try again');
+                // still next promise resolves and I get undefined in HTML :)
+            }
+            return response.json();
+        })
+        .then(data => {
+            divCity.innerText = data.name;
+            divDescription.innerText = data.weather[0].main;
+            divDate.innerText = new Date().toLocaleString();
+            divTemp.innerText = Number(data.main.temp - 273.15).toFixed(0);
+            let icon = data.weather[0].icon;
+            divIcon.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            divPressure.innerText = data.main.pressure + ' hPa';
+            divHumidity.innerText = data.main.humidity + ' %';
+            divWindSpeed.innerText = data.wind.speed + ' m/s';
+        })
+    fetch(API_URL2)
+        .then(response => response.json())
+        .then(data => { // this is a mess ))    COPY-PASTING IS A BAD HABIT )  but no time to clean this up Now, sorry ))) I'll refactor it later!
+            divHourlyTemp[0].innerText = data.list[0].dt_txt.slice(12, 16) + ' - ' + Number(data.list[0].main.temp - 273.15).toFixed(0) + '°C';
+            divHourlyTemp[1].innerText = data.list[1].dt_txt.slice(12, 16) + ' - ' + Number(data.list[1].main.temp - 273.15).toFixed(0) + '°C';
+            divHourlyTemp[2].innerText = data.list[2].dt_txt.slice(12, 16) + ' - ' + Number(data.list[2].main.temp - 273.15).toFixed(0) + '°C';
+            divHourlyTemp[3].innerText = data.list[3].dt_txt.slice(12, 16) + ' - ' + Number(data.list[3].main.temp - 273.15).toFixed(0) + '°C';
+            divHourlyTemp[4].innerText = data.list[4].dt_txt.slice(12, 16) + ' - ' + Number(data.list[4].main.temp - 273.15).toFixed(0) + '°C';
+        })
+    searchinput.value = '';
     searchinput.focus();
 }
-// class Weather {
-//     HTMLCreator(card){
-//         this.city = data.name;
-//         this.description = data.weather[0]+description      
-//     }
-// }
 
-*/
+function initialSearch() {
+    let API_URL0 = `${API_currentWeather}?q=Brussels&${API_key}`;
+    let API_URL0hourly = `${API_forecast}?q=Brussels&${API_key}&cnt=6`;
+    fetch(API_URL0)
+        .then(response => response.json())
+        .then(data => {
+            divCity.innerText = data.name;
+            divDescription.innerText = data.weather[0].main;
+            divDate.innerText = new Date().toLocaleString();
+            divTemp.innerText = Number(data.main.temp - 273.15).toFixed(0);
+            let icon = data.weather[0].icon;
+            divIcon.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            divPressure.innerText = data.main.pressure + ' hPa';
+            divHumidity.innerText = data.main.humidity + ' %';
+            divWindSpeed.innerText = data.wind.speed + ' m/s';
+        })
+    fetch(API_URL0hourly)
+        .then(response => response.json())
+        .then(data => { // this is a mess ))    COPY-PASTING IS A BAD HABIT )  but no time to clean this up Now, sorry ))) I'll refactor it later!
+            divHourlyTemp[0].innerText = data.list[0].dt_txt.slice(12, 16) + ' - ' + Number(data.list[0].main.temp - 273.15).toFixed(0) + '°C';
+            divHourlyTemp[1].innerText = data.list[1].dt_txt.slice(12, 16) + ' - ' + Number(data.list[1].main.temp - 273.15).toFixed(0) + '°C';
+            divHourlyTemp[2].innerText = data.list[2].dt_txt.slice(12, 16) + ' - ' + Number(data.list[2].main.temp - 273.15).toFixed(0) + '°C';
+            divHourlyTemp[3].innerText = data.list[3].dt_txt.slice(12, 16) + ' - ' + Number(data.list[3].main.temp - 273.15).toFixed(0) + '°C';
+            divHourlyTemp[4].innerText = data.list[4].dt_txt.slice(12, 16) + ' - ' + Number(data.list[4].main.temp - 273.15).toFixed(0) + '°C';
+        })
+}
